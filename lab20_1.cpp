fa9a7db21910aad49ce5e25067039aba57263401
godmonera <borwonpak_du@cmu.ac.th>
2023-02-21 17:15:44 +0700

    finished

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,17 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1,Rect R2){
 
+double x1 = max(R1.x,R2.x);
+double x2 = min(R1.x+R1.w,R2.x+R2.w);
 
+double y1 = min(R1.y,R2.y);
+double y2 = max(R1.y-R1.h,R2.y-R2.h);
 
+double widght = x1-x2;
+double height = y2-y1;
+double area = widght*height;
+if(area < 0) return 0;
+else return area;
 }
--- lab20_3.cpp
+++ lab20_3.cpp
@@ -9,7 +9,11 @@ using namespace std;
 
 struct student{
 
-    //Define struct student with four member (name ,id , gender, gpa);
+    char name[50] ;
+	int id;
+	char gender;
+	float gpa;
+	//Define struct student with four member (name ,id , gender, gpa);
     
 };
 
@@ -20,7 +24,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ //There is something wrong in this line.
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -62,8 +66,10 @@ int main(){
 	string textline;
 	
 	while(getline(student_file,textline)){
-		student s; 
+		student s;
+		char format[] ="%[^,],%d,%c,%f";
 		
+		sscanf(textline.c_str(),format,s.name,&s.id,&s.gender,&s.gpa);
 		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
 
 		allstudents.push_back(s); 		
@@ -84,6 +90,7 @@ int main(){
 				state = 3;
 			}else{
 			
+            allcourses[allcourses.size()-1].lecture_list.push_back(textline);
 			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
 			    
 			}			
@@ -92,7 +99,7 @@ int main(){
 				state = 1;
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
+			allcourses[allcourses.size()-1].student_list.push_back(p);
 				//Append (push_back) p to student_list of the recently added course in allcourses[];
 				
 			}
